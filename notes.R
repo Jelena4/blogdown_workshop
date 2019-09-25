7eb64740a94494507c6fdd629db6ff5c4ebb9465
Jelena Scepanovic <scepanovic@uchicago.edu>
2019-09-24 23:56:33 -0500

    added photo

--- notes.R
+++ notes.R
@@ -16,3 +16,28 @@ new_site(theme = "gcushen/hugo-academic",
 #### To remove demo:
 # go to files > home > demo
 # change active = false
+
+#### To remove demo from menu:
+# go to files > config and delete menu
+
+#### In the themes folder > hugo_academic,
+# you can find preinstalled example files with a theme
+# (in case you delete e. g. demo and you want it back)
+# treat themes folder as read only
+# so that you can always copy it to your content folder from the theme folder
+# Don't ever touch the public folder, only config and content.
+
+file.create('netlify.toml')
+
+#### Copy this into 
+# [build]
+# publish = "public"
+# command = "hugo"
+# 
+# [context.production.environment]
+# HUGO_VERSION = "0.55.6"
+# HUGO_ENV = "production"
+# HUGO_ENABLEGITINFO = "true"
+# 
+# [context.branch-deploy.environment]
+# HUGO_VERSION = "0.55.6"
